29846e206e30bc131aa0c08c8a474f1c60374bd2
dongxiaohao <2431752775@qq.com>
2019-03-28 22:04:52 +0800

    kou_exp

---
+++ kou_exp/test/rest/src/io_test/io_test.java
@@ -0,0 +1,13 @@
+package io_test;
+
+import java.io.*;
+
+public class io_test {
+	public static void main( String args[]) throws IOException {
+		
+		InputStream f = new FileInputStream("C:/Users/lenovo/Desktop/io_test/in_test.txt");
+	 
+	 
+	 
+	}
+}
